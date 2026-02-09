80c92b52195deee89f593ca92318922e17a98f47
Mag <166319982+magali-la@users.noreply.github.com>
2026-02-08 22:11:06 -0500

    feat(users): add login route with bcrypt password comparison and error handling

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -20,4 +20,39 @@ router.post("/register", async (req, res) => {
     }
 });
 
+// LOGIN - POST /api/users/login
+router.post("/login", async (req, res) => {
+    try {
+        // bad request error if email or password are missing
+        if (!req.body.email || !req.body.password) {
+            return res.status(400).json({ message: "Email and password are required." });
+        }
+
+        const user = await User.findOne({ email: req.body.email });
+
+        // user not found by email
+        if (!user) {
+            return res.status(400).json({ message: "Incorrect email or password."});
+        }
+
+        // check if correct password
+        const correctPw = await user.isCorrectPassword(req.body.password);
+
+        if (!correctPw) {
+            return res.status(400).json({ message: "Incorrect email or password." });
+        }
+
+        // convert mongoose document to js object and remove password from response
+        const userObj = user.toObject();
+        delete userObj.password;
+
+        // response with user
+        res.json({ user: userObj });
+
+    } catch (error) {
+        // if the bad request errors above aren't caught, then it's a server error
+        res.status(500).json({ message: error.message });
+    }
+});
+
 module.exports = router;
