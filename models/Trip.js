b0fa8655b342cb728af82bf9f0f3cc3f63d9ea17
Mag <166319982+magali-la@users.noreply.github.com>
2026-02-16 12:38:21 -0500

    feat(trips): add Trip schema with participants, activities, and status management

---
+++ models/Trip.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const tripSchema = new mongoose.Schema ({
+    title: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+    city: {
+        type: String,
+        enum: ["nyc", "atlanta", "lyon"],
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    isExact: {
+        type: Boolean
+    },
+    // status for labels and filtering of trips
+    status: {
+        type: String,
+        enum: ["planning", "upcoming", "ongoing", "archived"],
+        default: "planning",
+        required: true
+    },
+    // participants are a reference to the user ids, use it to populate the full user object in the routes to display their info in the trip details
+    participants: {
+        type: [mongoose.SchemaTypes.ObjectId],
+        ref: 'User',
+        default: []
+    },
+    // reference activities so that the full activity object can be populated in the route to display the details
+    activities: {
+        type: [mongoose.SchemaTypes.ObjectId],
+        ref: 'Activity',
+        default: []
+    }
+}, { timestamps: true });
+
+const Trip = mongoose.model('Trip', tripSchema);
+
+module.exports = Trip;
